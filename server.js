731a0881a4fafc9bf07b0c570aaccbe5462824e7
MRHVSD01 <harshvardhansinghdhannawat@gmail.com>
2026-01-30 00:42:51 +0530

    Fix MongoDB connection using MONGO_URI env variable

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const cors = require("cors");
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .catch(err => console.error("MongoDB Error:", err.message));
+
 
 app.use("/api", require("./routes/attendanceRoutes"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
+
