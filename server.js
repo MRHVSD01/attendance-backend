05ae14139c63c74c0c883d7ad813aa88267a5801
MRHVSD01 <harshvardhansinghdhannawat@gmail.com>
2026-01-30 00:52:57 +0530

    Fix require dotnev prolem remove some lines

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-dotenv.config();
 const app = express();
 
 app.use(cors({
